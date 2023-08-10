2d8ba04c8b95d7c66d1f373bb15dc9e8530b4c8a
Gabriel Dominguez Duran <gabrieldominguezduran@gmail.com>
2023-08-10 14:09:05 +0200

    Add emojis and style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,71 +1,40 @@
 <script>
-  let name = "Gabo";
-  let age = 48;
-  let title = "";
-  let description = "";
+  import { emoji } from "./emojis";
 
-  const capitalizeName = (string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  };
+  let state = "start";
+  let size = 20;
+  let grid = createGrid();
+  let maxMatches = grid.length / 2;
+  let selected = [];
+  let matches = [];
 
-  function incrementAge() {
-    age++;
+  function createGrid() {
+    let cards = new Set();
+    let maxSize = size / 2;
+
+    while (cards.size < maxSize) {
+      const randomIndex = Math.floor(Math.random() * emoji.length);
+      cards.add(emoji[randomIndex]);
+    }
+    return shuffle([...cards, ...cards]);
   }
 
-  function decrementAge() {
-    age--;
-  }
-
-  function handleNameChange(event) {
-    name = capitalizeName(event.target.value);
+  function shuffle(array) {
+    return array.sort(() => Math.random() - 0.5);
   }
 </script>
 
-<main>
-  <section class="buttons">
-    <h1>
-      Hello, my name is {name}{name.length > 3 ? "!" : ""} and I'm {age} years old!
-    </h1>
-    <input type="text" placeholder="Change name" on:input={handleNameChange} />
-    <input type="text" placeholder="Add Job title" bind:value={title} />
-    <input type="text" placeholder="Add description" bind:value={description} />
-    <button type="button" on:click={incrementAge}>Increment age</button>
-    <button type="button" on:click={decrementAge}>Decrement age</button>
-  </section>
-  <section class="contact-card" />
-</main>
+{#if state === "start"}
+  <h1>Matching game</h1>
+  <button on:click={() => (state = "playing")}>Play</button>
+{/if}
 
-<style>
-  main {
-    height: 95dvh;
-    border: 5px solid crimson;
-  }
-
-  .buttons {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: start;
-  }
-  h1 {
-    color: crimson;
-    text-align: center;
-  }
-
-  button {
-    background-color: crimson;
-    color: white;
-    border: none;
-    padding: 10px 20px;
-    border-radius: 5px;
-    margin: 10px;
-    cursor: pointer;
-  }
-
-  input {
-    padding: 10px;
-    border-radius: 5px;
-    border: 1px solid crimson;
-    margin: 10px;
-  }
-</style>
+{#if state === "playing"}
+  <div class="cards">
+    {#each grid as card}
+      <button class="card">
+        <div>{card}</div>
+      </button>
+    {/each}
+  </div>
+{/if}
